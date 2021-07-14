3a20268dc4364b878e5f01c16636ca716dbca228
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-07-14 08:42:18 -0400

    remove pattern

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,10 +1,9 @@
 from django.conf import settings
-# from django.conf.urls import patterns, include, url
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 
 
-urlpatterns = ['',
+urlpatterns = [
     url(r'^', include('accounting.apps.connect.urls',
         namespace="connect")),
     url(r'^books/', include('accounting.apps.books.urls',
